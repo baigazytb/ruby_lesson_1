da82bab57221273a3be28232c3003fb690fad012
Baigazy <baigazy.torobekov@gmail.com>
2021-03-05 07:00:10 +0600

    Add files via upload

---
+++ task_1.rb
@@ -0,0 +1,10 @@
+# task #1
+
+puts "What your name?"
+name = gets.chomp
+puts "Don't say, let me on letter \n-OK!\n-What your last name?"
+last_name = gets.chomp
+puts "This your last name? #{last_name}? \n-Realy? \n-Xm!"
+puts "So! How old are you?"
+age = gets.chomp
+puts "You #{name} #{last_name}, and #{age} years old."
